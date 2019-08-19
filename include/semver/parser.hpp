c830b8d07132ae743b0b6747a73f21e680ee212c
matken11235 <26405363+matken11235@users.noreply.github.com>
2019-08-19 09:33:15 +0900

    Update include/*

---
+++ include/semver/parser.hpp
@@ -0,0 +1,9 @@
+#ifndef SEMVER_PARSER_HPP
+#define SEMVER_PARSER_HPP
+
+#include <semver/parser/lexer.hpp>
+#include <semver/parser/parser.hpp>
+#include <semver/parser/range.hpp>
+#include <semver/parser/token.hpp>
+
+#endif // !SEMVER_PARSER_HPP
